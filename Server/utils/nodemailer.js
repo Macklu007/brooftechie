4e3166e668c514bfa43ecc90d5a3143515e9e4fe
Raunak Tiwary <macklutiwary@gmail.com>
2026-02-07 16:22:50 +0530

    fix: improve error handling in mail sender function

--- Server/utils/nodemailer.js
+++ Server/utils/nodemailer.js
@@ -1,53 +1,34 @@
-const nodemailer=require("nodemailer");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-async function mailsender(email,title,body)
-    
-    {
+async function mailsender(email, title, body) {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: false, // true only for port 465
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false, // fixes TLS handshake issues on Render
+      },
+    });
 
+    let info = await transporter.sendMail({
+      from: `"Bro of Techie" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: title,
+      html: body,
+    });
 
-    try{
-
-      console.log(email);
-       
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            auth: {
-              user: process.env.MAIL_USER,
-              pass: process.env.MAIL_PASS,
-            },
-            secure: false,
-
-
-
-           
-            
-            
-
-        });
-
-        let info= await transporter.sendMail({
-            from:`"Bro of techie"  <${process.env.MAIL_USER}>`,
-            to:`${email}`,
-            subject:`${title}`,
-            html:`${body}`,
-        });
-
-        console.log(info.response,"nodemailer mei aagye");
-
-        return info;
-            
-
-    }
-    
-  catch(err){
-
-    console.log(err.message)
-    return err.message
+    console.log("Mail sent:", info.messageId);
+    return info;
+  } catch (err) {
+    console.error("Mail error:", err);
+    throw err;
   }
-  
-
 }
 
-
-module.exports=mailsender;
+module.exports = mailsender;
